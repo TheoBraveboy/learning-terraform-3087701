23007c3d081f9186a9a230fc62d65cda006e43eb
TheoBraveboy <theo.braveboy@hotmail.com>
2023-02-28 10:14:19 -0500

    2/28/23

--- main.tf
+++ main.tf
@@ -34,7 +34,8 @@ module "vpc" {
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
+  
+  subnet_id              = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -44,10 +45,9 @@ resource "aws_instance" "blog" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  
   version = "4.17.1"
+  
   name    = "blog"
-  description = "Creating security group module"
   vpc_id  = module.vpc.public_subnets[0]
   
   ingress_rules       = ["http-80-tcp","https-443-tcp"]

833e3ee85e914846afb398c38d4eab9867d85f0c
TheoBraveboy <theo.braveboy@hotmail.com>
2023-03-02 11:55:56 -0500

    test

--- main.tf
+++ main.tf
@@ -95,7 +95,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.1"
   
-  name    = ${var.environment.name}-blog"
+  name    = "${var.environment.name}-blog"
   vpc_id  = module.vpc.vpc_id
   
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
